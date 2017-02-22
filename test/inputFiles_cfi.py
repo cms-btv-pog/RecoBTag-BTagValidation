84d537ac8509c34902d540e12c31918ef43610c8
Devdatta <devdatta.majumder@cern.ch>
2017-02-23 00:01:33 +0100

    input

--- test/inputFiles_cfi.py
+++ test/inputFiles_cfi.py
@@ -1,6 +1,7 @@
 FileNames = {
   'test':[
-    'file:/afs/cern.ch/work/d/devdatta/CMSREL/BTagging/CMSSW_8_0_23/src/RecoBTag/PerformanceMeasurements/test/JetTree_mc_FatJets_Subjets_BoostedCommissioning.root',
+    #'file:/afs/cern.ch/work/d/devdatta/CMSREL/BTagging/CMSSW_8_0_23/src/RecoBTag/PerformanceMeasurements/test/JetTree_mc_FatJets_Subjets_BoostedCommissioning.root',
+    'file:/afs/cern.ch/user/d/devdatta/eos/cms/store/group/phys_btag/BoostedBTag/BTagNTuples/8_0_X_v2.03/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/QCD_Pt_1000-Inf_MuEnriched_mcJPcalib_12616_v4_full/170111_203007/0000/JetTree_mc_FatJets_Subjets_BoostedCommissioning_1.root'
     ],
   'FileNames':[
     ],
