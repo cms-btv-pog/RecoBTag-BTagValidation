bed5f984739f2e509aefd7f898e21708c058232e
Devdatta <devdatta.majumder@cern.ch>
2018-06-04 18:55:16 +0200

    batch options

--- test/createAndSubmitJobs.py
+++ test/createAndSubmitJobs.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import sys, os, shutil, re, subprocess
-from optparse import OptionParser
+#from optparse import OptionParser
+from argparse import ArgumentParser 
 
 
 def make_filenamelist(input_dir):
@@ -60,6 +61,16 @@ INPUT_FILES
 ]
 """
 
+condor_template = """universe              = vanilla
+executable            = EXEC
+arguments             = $(ClusterID) $(ProcId)
+output                = OUTPUT/job_JOB_NUMBER.$(ClusterId).$(ProcId).out
+error                 = OUTPUT/job_JOB_NUMBER.$(ClusterId).$(ProcId).err
+log                   = OUTPUT/job_JOB_NUMBER.$(ClusterId).log
++JobFlavour           = "QUEUE"
+queue
+"""
+
 
 bash_template = """#!/bin/bash
 
@@ -96,26 +107,30 @@ For more help: ./createAndSubmitJobs.py --help
 
 def main():
   # input parameters
-  parser = OptionParser(usage=usage)
+  #parser = OptionParser(usage=usage)
+  parser = ArgumentParser(description=usage)
 
-  parser.add_option("-w", "--main_workdir", dest="main_workdir", action='store', help="Main working directory", metavar="MAIN_WORKDIR")
-  parser.add_option("-d", "--dataset_list", dest="dataset_list", action='store', help="Text file containing a list of datasets to be processed", metavar="DATASET_LIST")
-  parser.add_option('-m', '--match', dest="match", action='store', help='Only files containing the MATCH string in their names will be considered (This parameter is optional)', metavar='MATCH')
-  parser.add_option("-c", "--cmssw_cfg", dest="cmssw_cfg", action='store', help="CMSSW configuration file", metavar="CMSSW_CFG")
-  parser.add_option('-f', '--fraction', dest='fraction', action='store', default='1.0', help='Fraction of files to be processed. Default value is 1 (This parameter is optional)', metavar='FRACTION')
-  parser.add_option("-q", "--queue", dest="queue", action='store', default='8nh', help="LXBatch queue (choose among cmst3 8nm 1nh 8nh 1nd 1nw). Default is '8nh' (This parameter is optional)", metavar="QUEUE")
-  parser.add_option("-n", "--no_submission", dest="no_submission", action='store_true', default=False, help="Create the necessary configuration files and skip the job submission (This parameter is optional)")
+  parser.add_argument("-w", "--main_workdir", dest="main_workdir", action='store', help="Main working directory", metavar="MAIN_WORKDIR")
+  parser.add_argument("-d", "--dataset_list", dest="dataset_list", action='store', help="Text file containing a list of datasets to be processed", metavar="DATASET_LIST")
+  parser.add_argument('-m', '--match', dest="match", action='store', help='Only files containing the MATCH string in their names will be considered (This parameter is optional)', metavar='MATCH')
+  parser.add_argument("-c", "--cmssw_cfg", dest="cmssw_cfg", action='store', help="CMSSW configuration file", metavar="CMSSW_CFG")
+  parser.add_argument('-f', '--fraction', dest='fraction', action='store', default='1.0', help='Fraction of files to be processed. Default value is 1 (This parameter is optional)', metavar='FRACTION')
+  parser.add_argument("-q", "--queue", dest="queue", action='store', default='8nh', help="LXBatch/condor queue (choose among cmst3 8nm 1nh 8nh 1nd 1nw). Default is '8nh' (This parameter is optional)", metavar="QUEUE")
+  parser.add_argument("-n", "--no_submission", dest="no_submission", action='store_true', default=False, help="Create the necessary configuration files and skip the job submission (This parameter is optional)")
+  parser.add_argument("-b", "--batch_option", dest="batch_option", action='store', default='lxbatch', help="Select lxbatch or condor as the batch submission option. You may need to adjust the batch queue accordingly (This parameter is optional)")
 
-  (options, args) = parser.parse_args()
+  #(options, args) = parser.parse_args()
+  args = parser.parse_args()
+  if args.batch_option == 'condor' and args.queue == '': args.queue = 'tomorrow'
 
   # make sure all necessary input parameters are provided
-  if not (options.main_workdir and options.dataset_list and options.cmssw_cfg):
+  if not (args.main_workdir and args.dataset_list and args.cmssw_cfg):
     print usage
     sys.exit()
 
-  main_workdir = options.main_workdir
-  dataset_list = options.dataset_list
-  cmssw_cfg = options.cmssw_cfg
+  main_workdir = args.main_workdir
+  dataset_list = args.dataset_list
+  cmssw_cfg = args.cmssw_cfg
 
   # redefine main_workdir as an absolute path (if not defined in such form already)
   if not re.search("^/", main_workdir):
@@ -177,11 +192,11 @@ def main():
     os.mkdir(os.path.join(dataset_workdir,'output'))
 
     filelist = []
-    process_input_dir(line_elements[2], options.match, filelist)
+    process_input_dir(line_elements[2], args.match, filelist)
 
     ##################################################
     njobs = line_elements[1]
-    numfiles = int(len(filelist)*float(options.fraction))
+    numfiles = int(len(filelist)*float(args.fraction))
     ijobmax=int(njobs)
     if ijobmax > numfiles:
        ijobmax=numfiles
@@ -221,9 +236,25 @@ def main():
        bash_script.write(bash_script_content)
        bash_script.close()
 
-       if(not options.no_submission):
-         cmd = 'bsub -q ' + options.queue + ' -o ' + os.path.join(dataset_workdir,'output','job_' + str(ijob) + '.log') + ' source ' + os.path.join(dataset_workdir,'input','job_' + str(ijob) + '.sh')
-         os.system(cmd)
+       if args.batch_option == 'condor':
+         condor_script = open(os.path.join(dataset_workdir,'input','job_' + str(ijob) + '.condor'),'w')
+         #condor_script_content = re.sub('EXEC',os.path.join(dataset_workdir,'input','job_' + str(ijob) + '.sh'),condor_template)
+         condor_script_content = re.sub('EXEC','job_' + str(ijob) + '.sh',condor_template)
+         condor_script_content = re.sub('JOB_NUMBER',str(ijob),condor_script_content)
+         condor_script_content = re.sub('OUTPUT',os.path.join(dataset_workdir,'output'),condor_script_content)
+         condor_script_content = re.sub('QUEUE',args.queue,condor_script_content)
+         condor_script.write(condor_script_content)
+         condor_script.close()
+
+       if(not args.no_submission):
+         if args.batch_option == 'lxbatch':
+           cmd = 'bsub -q ' + args.queue + ' -o ' + os.path.join(dataset_workdir,'output','job_' + str(ijob) + '.log') + ' source ' + os.path.join(dataset_workdir,'input','job_' + str(ijob) + '.sh')
+           os.system(cmd)
+         elif args.batch_option == 'condor':
+           os.chdir(os.path.join(dataset_workdir,'input'))
+           cmd = 'condor_submit ' +  os.path.join(dataset_workdir,'output','job_' + str(ijob) + '.log') + ' source ' + os.path.join(dataset_workdir,'input','job_' + str(ijob) + '.condor')
+           os.system(cmd)
+           os.chdir(cfg_dirname)
 
   # close all open files
   dataset_list_file.close()
