240b978dadca17fc76bc11edaf92b2088901c5f8
Devdatta <devdatta.majumder@cern.ch>
2018-10-08 14:06:11 +0200

    input

--- test/inputFiles_cfi.py
+++ test/inputFiles_cfi.py
@@ -1,3 +1,4 @@
 FileNames = [
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/BTagNTuples/2017/9_4_X/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCP5_13TeV_pythia8/QCD_Pt_1000toInf_MuEnriched_v1_Jan3-2018/180103_174700/0000/JetTree_mc_FatJets_Subjets_BoostedCommissioning_1.root',
+    '/eos/cms/store/group/upgrade/devdatta/JetTree_PU200_mc_FatJets_Subjets_BoostedCommissioning.root',
+    #'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/BTagNTuples/2017/9_4_X/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCP5_13TeV_pythia8/QCD_Pt_1000toInf_MuEnriched_v1_Jan3-2018/180103_174700/0000/JetTree_mc_FatJets_Subjets_BoostedCommissioning_1.root',
 ]
