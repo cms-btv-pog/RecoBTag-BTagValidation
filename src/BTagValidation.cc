ea862a3da80441b83bd74d54911ff3fd94a10eab
Devdatta <devdatta.majumder@cern.ch>
2021-03-09 14:50:54 +0100

    UL17 WPs

--- src/BTagValidation.cc
+++ src/BTagValidation.cc
@@ -245,8 +245,8 @@ const std::map<std::string, std::pair<int, int>> BTagValidation::pts_ = {
 const std::vector<std::string> BTagValidation::sels_ = {"pass", "fail"}; 
 
 ////https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation102X
-const double BTagValidation::DeepCSVL_ = 0.1241;
-const double BTagValidation::DeepCSVM_ = 0.4184; 
+const double BTagValidation::DeepCSVL_ = 0.1355;
+const double BTagValidation::DeepCSVM_ = 0.4506; 
 
 //
 // constructors and destructor
